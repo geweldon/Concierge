e63ee64484cb0bf0486bb38621859dbb626b51b6
Grant Weldon <geweldon@gmail.com>
2018-04-07 00:34:02 -0400

    no message

--- Concierge/urls.py
+++ Concierge/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from . import views
+from .views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
