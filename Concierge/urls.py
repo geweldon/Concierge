be36e91c29791292afdff573082588350d0f070e
Grant Weldon <geweldon@gmail.com>
2018-04-06 23:34:35 -0400

    hunting down the problem

--- Concierge/urls.py
+++ Concierge/urls.py
@@ -17,7 +17,6 @@ from django.conf.urls import url
 from django.urls import path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
-from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
