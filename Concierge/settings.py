07895b71bb64e308f7ea1b2e53a620803340af68
Grant Weldon <geweldon@gmail.com>
2018-04-06 20:49:32 -0400

    trying to make heroku happy

--- Concierge/settings.py
+++ Concierge/settings.py
@@ -106,6 +106,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = ['Front-end']
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
